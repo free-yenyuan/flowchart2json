453e1c2e97270cd9568271cb493236fdac1ebafa
yenyuan <yenyuan@foxmail.com>
2024-01-13 20:35:10 +0800

    Add Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,26 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Nav } from "./nav.js";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Script Toolbox',
-  description: '',
-}
+    title: "Script Toolbox",
+    description: "",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className='w-screen h-screen'>
-      <body className={inter.className + 'w-full h-full grid grid-cols-1 place-items-center '}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en" className="w-screen h-screen">
+            <body
+                className={
+                    inter.className +
+                    "w-full h-full grid grid-cols-1 place-items-center "
+                }
+            >
+                <Nav />
+                {children}
+            </body>
+        </html>
+    );
 }
---
+++ app/nav.js
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+
+export function Nav() {
+    const menuList = [
+        {
+            title: "Home",
+            url: "/",
+        },
+        {
+            title: "Branch Tool",
+            url: "/branchtool",
+        },
+    ];
+    return (
+        <nav className="absolute bg-slate-100 shadow-md h-20 w-full top-0 left-0">
+            <div
+                id="title-bar"
+                className="float-left px-5 h-full content-center grid"
+            >
+                <Link className="inline-block mt-auto font-semibold" href="/">
+                    PlayMe Toolbox
+                </Link>
+            </div>
+            <div
+                id="menu-bar"
+                className={`w-1/2 h-full gap-x-4 flex align-middle float-right justify-end mr-5`}
+            >
+                {menuList.map((value, index) => (
+                    <Link
+                        href={value.url}
+                        key={index}
+                        className="flex flex-wrap content-center text-center"
+                    >
+                        {value.title}
+                    </Link>
+                ))}
+            </div>
+        </nav>
+    );
+}
